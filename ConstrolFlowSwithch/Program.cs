2a970956ae8a805a03e39648436ee764b2131538
SangYeon Lee <sylee2002@gmail.com>
2017-09-20 16:30:31 +0900

    If and Switch
    컨디션이 여러개면 if 명확하면 switch 사용

---
+++ ConstrolFlowSwithch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstrolFlowSwithch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "Avetis":
+                    Console.WriteLine("This is my name.");
+                    break;  // 브레이크가 여기 없으면 위 조건을 만족하여 아래 브레이크의 결과를 보여준다.
+                case "Visual Studio":
+                    Console.WriteLine("This is my IDE.");
+                    break;
+                default:
+                    Console.WriteLine("This is no match.");
+                    break;
+
+            }
+        }
+    }
+}
--- ControlFlowDecision/Program.cs
+++ ControlFlowDecision/Program.cs
@@ -10,6 +10,23 @@ namespace ControlFlowDecision
     {
         static void Main(string[] args)
         {
+            string player = Console.ReadLine();
+
+            if (player.Equals("AI"))
+            {
+                Console.WriteLine("The player is AI.");
+            }
+            else if (player.Equals("Human"))
+            {
+                Console.WriteLine("The player is Human.");
+            }
+            else
+            {
+                Console.WriteLine("The player is not AI and not Human.");
+            }
+
+
+
         }
     }
 }
