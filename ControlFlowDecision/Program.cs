2b0df8cc1c733847429dd5b4d7d27afb584c57fa
SangYeon Lee <sylee2002@gmail.com>
2017-09-20 15:22:26 +0900

    studied

---
+++ ControlFlowDecision/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlFlowDecision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Variable/Variable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfBooks = 1;
+            string myName = "Avetis";
+            bool IsCompatible = true;
+            string number = "2";
+            int numberConverted = Convert.ToInt32(number);
+
+            Console.WriteLine(countOfBooks);
+            Console.WriteLine(myName);
+            Console.WriteLine(IsCompatible);
+            Console.WriteLine(number);
+            Console.WriteLine(numberConverted);
+        }
+    }
+}
