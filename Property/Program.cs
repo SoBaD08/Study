7ea6b8194de99255572823128e6a38525dc40d74
SangYeon Lee <sylee2002@gmail.com>
2017-09-21 17:39:09 +0900

    Property
    Porpoerty and Field in class

---
+++ Property/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            player.Lives = 100;
+
+            Console.WriteLine(player.Lives);
+        }
+    }
+}
