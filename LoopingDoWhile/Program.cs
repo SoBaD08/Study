c8b4a4448d168a8cd6d1ff40e0e734775efbf9ef
SangYeon Lee <sylee2002@gmail.com>
2017-09-21 15:01:27 +0900

    While and Do While

---
+++ LoopingDoWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopingDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string input2 = string.Empty;
+
+            do // do while과 while 의 차이는 do 는 최소 한번은 실행 됨.
+            {
+                input2 = Console.ReadLine();
+                Console.WriteLine("Your input is {0}.", input2);
+
+            } while (!input2.Equals(string.Empty));
+        }
+    }
+}
---
+++ LoopingWhile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopingWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+            while (index < 10)
+            {
+                Console.WriteLine("The number is {0}", index);
+                index = index + 1;
+            }
+        }
+    }
+}
