9c3603e9091d408b403ffdb2046e199838926379
SangYeon Lee <sylee2002@gmail.com>
2017-09-21 15:14:56 +0900

    For loop
    branching statement are used "break" and "continue"

---
+++ LoopingFor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopingFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 1) continue;  
+                // braching statement break or continue can be used here.  continue will not print out the line when i = 1
+
+                Console.WriteLine("This i is {0}.", i);
+            }
+        }
+    }
+}
